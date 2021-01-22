bbd960a52010d43c7a5130a49ee3316256c3acb2
NabilHOSSINI <77832920+NabilHOSSINI@users.noreply.github.com>
2021-01-22 11:44:58 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 formation_poec = input('Entrez le nom de votre formation POEC 2020')
 if formation_poec == 'DevOps':
   print('Vous êtes les meilleurs !')
+else:
+  print('Soyons pas méchant, bravo à vous aussi :)')
+  
